b083d5e6c88bfcf57c8acc3a48b6b99a838e27f5
rtocastro <ricktorres@live.com>
2024-03-28 00:03:11 -0700

    formatted footer

--- dpsshelper/src/components/Footer.jsx
+++ dpsshelper/src/components/Footer.jsx
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom'
 
 
 export default function Footer() {
-  return (
-    <>
-    <Link to='javascript:void(0)'
-    onClick={() => window.location = 'mailto:ricardotorres2@dpss.lacounty.gov'}>
-        
-     <h5>Developed by Ricardo Torres</h5>
-    </Link>
- 
-    </>
-  )
+    return (
+        <>
+            <Link to='javascript:void(0)'
+                onClick={() => window.location = 'mailto:ricardotorres2@dpss.lacounty.gov'}>
+                <h5>Developed by Ricardo Torres</h5>
+            </Link>
+
+        </>
+    )
 }

6fced1f867d3bb0cc5e35521996c89d27cac52d8
rtocastro <ricktorres@live.com>
2024-05-21 23:18:07 -0700

    possible fix?

--- journalhelper/src/pages/helper.jsx
+++ journalhelper/src/pages/helper.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 //import DropDownCF from '../components/DropDownCF';
 import CalQuest from '../components/CalQuest'
 
-export default function Helper(){
+export default function Helper() {
   return (
     <>
      <h2>Create a journal entry</h2>

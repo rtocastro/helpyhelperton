d484dda26cb57565785d8aac34c22e9f189e7d91
rtocastro <ricktorres@live.com>
2024-05-21 22:08:00 -0700

    testing again

--- journalhelper/vite.config.js
+++ journalhelper/vite.config.js
@@ -5,3 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+resolve: {
+  alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+}

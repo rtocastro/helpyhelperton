f78ea4ba086f86b389a8377444a2bb92f0101dbb
rtocastro <ricktorres@live.com>
2024-05-22 23:09:57 -0700

    change helper jsx to export at end

--- journalhelper/src/pages/helper.jsx
+++ journalhelper/src/pages/helper.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 //import DropDownCF from '../components/DropDownCF';
 import CalQuest from '../components/CalQuest'
 
-export default function Helper() {
+function Helper() {
   return (
     <>
-     <h2>Create a journal entry</h2>
-     <CalQuest />
+      <h2>Create a journal entry</h2>
+      <CalQuest />
     </>
   )
 }
+
+export default Helper;
